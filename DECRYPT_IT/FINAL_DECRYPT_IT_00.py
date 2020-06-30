0be6d347941b57e4617e56cd9ff89a433d505dd7
privateproxon <67372420+privateproxon@users.noreply.github.com>
2020-06-30 16:38:40 +0900

    Update FINAL_DECRYPT_IT_00.py

--- DECRYPT_IT/FINAL_DECRYPT_IT_00.py
+++ DECRYPT_IT/FINAL_DECRYPT_IT_00.py
@@ -3,15 +3,41 @@ from Crypto import Random
 
 import os, sys
 
+def decrypt_it(bytefile, key, iv):
+	cfb_decipher = AES.new(key, AES.MODE_CFB, iv)
+	return cfb_decipher.decrypt(bytefile)
+
+def readBinFile(dir):
+	file = open(dir, "rb")
+	data = file.read()
+	file.close()
+	return data
+
+def writeBinFile(dir, data):
+	file = open(dir, "wb")
+	file.write(data)
+	file.close()
+
+def safe_os(cmd):
+	try:
+		os.system(cmd)
+	except:
+		pass
+
 print ("The program allows you to decrypt files")
 print ("Please put the key file you have received from the sender in the current directory")
 temp = input ("Press \'Enter\' key to continue...\n")
+
+try:
+	plain_key = readBinFile("KEYIV.key")
+except:
+	raise Exception('KEYIV.key does not exist in the current directory')
 
 #Retrieving KEY and IV used for decryption
 print ("Retrieving KEYIV")
 
 try:
-	keyiv = open ("KEYIV.key", 'r')
+	keyiv = open("KEYIV.key", 'r')
 	ki = keyiv.readlines()
 	keyiv.close()
 except:
@@ -22,43 +48,27 @@ print ("Retrieving completed\n")
 exec("key = "+ ki[0][6:-2])
 exec("iv = " + ki[1][5:-2])
 
-try:
-	os.system('mkdir 1ENCRYPTED')
-except:
-	pass
+safe_os('mkdir 1ENCRYPTED')
+safe_os('mkdir 2DECRYPTED')
 
 print ("Please put the files you want to decrypt in \'1ENCRYPTED\'")
 temp = input ("Press \'Enter\' key to continue...")
 
 #Begin decryption
 print ("Retrieving files in 1ENCRYPTED")
-
 try:
 	inFiles = os.listdir("1ENCRYPTED")
 except:
 	raise Exception('Directory \'1ENCRYPTED\' does not exist in the current directory')
 
-print ("Retrieving completed")
-print ("Beginning Decryption\n")
+print ("Beginning Decryption...\n")
 
 for file in inFiles:
 	print ("Decrypting", file)
 
-	enc_file2 = open("1ENCRYPTED/"+file, "rb")
-	enc_data2 = enc_file2.read()
-	enc_file2.close()
+	filedata = readBinFile("1ENCRYPTED/"+file)
+	writeBinFile("2DECRYPTED/"+file[:-4], decrypt_it(filedata, key, iv))
 
-	cfb_decipher = AES.new(key, AES.MODE_CFB, iv)
-	plain_data = cfb_decipher.decrypt(enc_data2)
-
-	try:
-		os.system('mkdir 2DECRYPTED')
-	except:
-		pass
-
-	output_file = open("2DECRYPTED/"+file[:-4], "wb")
-	output_file.write(plain_data)
-	output_file.close()
 	print ("Completed decrypting", file, "\n")
 
-print ("Decrypting Completed\n")
+print ("Decryption Completed\n")
