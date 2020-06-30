9d0b3ff2df729e0fd904c7ecba39d824936e0466
privateproxon <67372420+privateproxon@users.noreply.github.com>
2020-06-30 16:37:51 +0900

    Update
    Function Clear, Comment, Efficient

--- ENCRYPT_IT/FINAL_ENCRYPT_IT_00.py
+++ ENCRYPT_IT/FINAL_ENCRYPT_IT_00.py
@@ -3,6 +3,40 @@ from Crypto import Random
 
 import os, sys
 
+def encrypt_it(bytefile, key, iv):
+	cfb_cipher = AES.new(key, AES.MODE_CFB, iv)
+	return cfb_cipher.encrypt(bytefile)
+
+def decrypt_it(bytefile, key, iv):
+	cfb_decipher = AES.new(key, AES.MODE_CFB, iv)
+	return cfb_decipher.decrypt(bytefile)
+
+def readBinFile(dir):
+	with open(dir, "rb") as file:
+		data = file.read()
+	file.close()
+	return data
+
+def writeBinFile(dir, data):
+	with open(dir, "wb") as file:
+		file.write(data)
+	file.close()
+
+def safe_os(cmd):
+	try:
+		os.system(cmd)
+	except:
+		pass
+
+def keyGenerate(key, iv):
+	with open("KEYIV.key", "w") as f:
+		f.write("key = %s \n iv = %s \n" %(key, iv))
+	f.close()
+
+	key_data = readBinFile("KEYIV.key")
+	writeBinFile("KEYIV.key", key_data)
+
+
 #KEYIV used for encrypting data
 key = Random.new().read(AES.block_size)
 iv = Random.new().read(AES.block_size) #initialization vector
@@ -10,70 +44,55 @@ iv = Random.new().read(AES.block_size) #initialization vector
 # print ("\nKEY:", key)
 # print ("IV:", iv, "\n")
 
-try:
-	os.system('mkdir 1ORIGINAL')
-except:
-	pass
+safe_os('mkdir 1ORIGINAL')
+safe_os('mkdir 2ENCRYPTED')
+safe_os('mkdir 3DECRYPTED')
 
 print ("The program allows you to encrypt files")
 print ("Please put the files you want to encrypt in \'1ORIGINAL\'")
+
 temp = input ("Press \'Enter\' key to continue...")
-
-
-print ("Retrieving files in 1ORIGINAL")
-
-try:
-	inFiles = os.listdir("1ORIGINAL")
-except:
-	raise Exception('Directory \'1ORIGINAL\' does not exist in the current directory')
-
-print ("Retrieving completed")
-
-print ("Beginning Encryption\n")
-
-for file in inFiles:
-	print ("Encrypting", file)
-	input_file = open("1ORIGINAL/"+file, "rb")
-	input_data = input_file.read()
-	input_file.close()
-
-	cfb_cipher = AES.new(key, AES.MODE_CFB, iv)
-	enc_data = cfb_cipher.encrypt(input_data)
-
-	try:
-		os.system('mkdir 2ENCRYPTED')
-	except:
-		pass
-
-	enc_file = open("2ENCRYPTED/"+file+".enc", "wb")
-	enc_file.write(enc_data)
-	enc_file.close()
-	print ("Encrypting Completed")
-
-	print ("Decrypting", file)
-	enc_file2 = open("2ENCRYPTED/"+file+".enc", "rb")
-	enc_data2 = enc_file2.read()
-	enc_file2.close()
-
-	cfb_decipher = AES.new(key, AES.MODE_CFB, iv)
-	plain_data = cfb_decipher.decrypt(enc_data2)
-
-	try:
-		os.system('mkdir 3DECRYPTED')
-	except:
-		pass
-
-	output_file = open("3DECRYPTED/"+file, "wb")
-	output_file.write(plain_data)
-	output_file.close()
-	print ("Completed decrypting", file, "\n")
-
-print ("Encryption successful\n")
 
 # Creating txt with key and iv used for encryption
 print ("Generating KEYIV for the recipient")
-f = open("KEYIV.key", "w+")
-f.write("key = %s \n" %key)
-f.write("iv = %s \n" %iv)
-f.close()
-print ("Completed Generating")
+keyGenerate(key, iv)
+
+
+print ("Retrieving files in 1ORIGINAL\n")
+try:
+	oriFile = os.listdir("1ORIGINAL")
+except:
+	raise Exception('Directory \'1ORIGINAL\' does not exist in the current directory')
+
+
+print ("Beginning Encryption...\n")
+
+for file in oriFile:
+	print ("Encrypting", file)
+
+	filedata = readBinFile("1ORIGINAL/"+file)
+	writeBinFile("2ENCRYPTED/"+file+".enc", encrypt_it(filedata, key, iv))
+
+	print ("Completed encrypting", file, "\n")
+
+print ("Encryption successful\n")
+
+
+print ("Retrieving files in 2ENCRYPTED\n")
+try:
+	inFiles = os.listdir("2ENCRYPTED")
+except:
+	raise Exception('Directory \'2ENCRYPTED\' does not exist in the current directory')
+
+
+print ("Beginning Decryption...\n")
+
+for file in inFiles:
+	print ("Decrypting", file)
+
+	filedata = readBinFile("2ENCRYPTED/"+file)
+	writeBinFile("3DECRYPTED/"+file[:-4], decrypt_it(filedata, key, iv))
+
+	print ("Completed decrypting", file, "\n")
+
+print ("Decryption successful\n")
